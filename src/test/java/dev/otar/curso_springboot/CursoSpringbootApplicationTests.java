581be0e461c42e2223349dc8248309faf21e4315
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-10-22 22:37:43 -0500

    Initial commit

---
+++ src/test/java/dev/otar/curso_springboot/CursoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.otar.curso_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
